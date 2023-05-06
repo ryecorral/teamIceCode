8b981068d2262ea09c79a8e0189047f22d61a5dd
Rye <126563338+ryecorral@users.noreply.github.com>
2023-05-06 11:06:44 +0800

    Add files via upload

---
+++ NODEpracticeFromYT/hello.js
@@ -0,0 +1,12 @@
+// function findMeaning() { return 42};
+// console.log(findMeaning());
+// Object.assign();
+
+// const mission ='learn';
+// if (mission === 'learn')
+// {
+//     console.log('Time to write some Node code!');
+// } else {
+//     console.log(`Is ${mission} really more fun?`);
+// }
+
